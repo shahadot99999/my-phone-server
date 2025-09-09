3f1e02d64fd80c80aaca5bf195a86f42fffe8a3c
shahadot99999 <shahadot99999@gmail.com>
2025-09-09 21:36:07 +0600

    nodemon are added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+var cors = require('cors')
 const phones = require('./phones.json');
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('My phone server is comming rapidly')
